36888440eabc56f61f539b36892c61088e282b60
emilk <emilk@10.128.198.117>
2019-10-31 13:56:34 +0100

    added test

--- iTunesAPI/src/window.java
+++ iTunesAPI/src/window.java
@@ -82,7 +82,7 @@ public class window extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
-        // TODO add your handling code here:
+        System.out.print("test");
     }//GEN-LAST:event_jButton1ActionPerformed
 
     /**
