b37a4c0b4207baefbd09d9d7b6715410c158260a
emilk <emilk@10.128.198.117>
2019-10-31 15:56:42 +0100

    begyndt at kode et card til de forskellige sange.

--- iTunesAPI/src/iTunesCard.java
+++ iTunesAPI/src/iTunesCard.java
@@ -1,3 +1,8 @@
+
+import java.awt.Color;
+import java.awt.GridBagLayout;
+import javax.swing.JPanel;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -9,5 +14,33 @@
  * @author emilk
  */
 public class iTunesCard {
+    public String cover;
+    public String preview;
+    public String song_name;
+    public String artist_name;
+    public String genre;
     
+    public int index;
+    
+    public iTunesCard (String coverin, String previewin, String song_namein, String artist_namein,String genrein){
+        cover=coverin;
+        preview=previewin;
+        song_name=song_namein;
+        artist_name=artist_namein;
+        genre=genrein;
+        index=indexin;
+    
+        JPanel trackcard = new JPanel(new GridBagLayout(), false);
+        trackcard.setBackground(Color.lightGray);
+        
+        trackcard.setBounds(10, 10+(index*100), 100, 749);
+    }
+    
+    public class UserDetail extends JPanel{
+        
+    }
 }
+    
+    
+    
+
